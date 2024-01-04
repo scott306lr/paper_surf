b34f7b5aae286b5e6dc5206b2d8602fa0fb76284
scott306lr <scott306lr@gmail.com>
2024-01-04 13:27:39 +0800

    split search tokens by commas

--- src/pages/final.tsx
+++ src/pages/final.tsx
@@ -19,32 +19,41 @@ import { type z } from "zod";
 export default function PaperSurf() {
   const [searchInput, setSearchInput] = useState("play chess");
   const [stopwordInput, setStopwordInput] = useState("");
-  const search_mutation = api.scholar.searchByInput.useMutation();
-  const { mutate: lda_mutate, data, isLoading } = api.scholar.lda.useMutation();
+  // const search_mutation = api.scholar.searchByInput.useMutation();
+  const {
+    mutateAsync: search_mutateAsync,
+    data: search_data,
+    isLoading: search_isLoading,
+  } = api.scholar.searchByInput.useMutation();
+  const {
+    mutate: lda_mutate,
+    data: lda_data,
+    isLoading: lda_isLoading,
+  } = api.scholar.lda.useMutation();
 
-  const handleLDA = async (
-    positive: string[],
-    negative: string[],
-    stopwords: string[],
-  ) => {
-    const search_result = await search_mutation.mutateAsync({
-      input: positive,
-      filter_input: negative,
-    });
+  // const handleLDA = async (
+  //   positive: string[],
+  //   negative: string[],
+  //   stopwords: string[],
+  // ) => {
+  //   const search_result = await search_mutateAsync({
+  //     input: positive,
+  //     filter_input: negative,
+  //   });
 
-    if (search_result != null)
-      lda_mutate({
-        paperID_array: to_lda(search_result),
-        stopwords: stopwords,
-      });
-  };
+  //   if (search_result != null)
+  //     lda_mutate({
+  //       paperID_array: to_lda(search_result),
+  //       stopwords: stopwords,
+  //     });
+  // };
 
   const onSubmit = async (values: z.infer<typeof inputFormSchema>) => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
 
-    const search_result = await search_mutation.mutateAsync({
+    const search_result = await search_mutateAsync({
       input: values.positive.split(", "),
       filter_input: values.negative.split(", "),
     });
@@ -56,7 +65,7 @@ export default function PaperSurf() {
       });
   };
 
-  const graph = keyWord_to_graph(data?.[1] ?? []);
+  const graph = keyWord_to_graph(lda_data?.[1] ?? []);
   const graphData: CGraphData = {
     nodes: graph.nodes.map((node) => ({
       id: `${node.topic}`,
@@ -82,41 +91,18 @@ export default function PaperSurf() {
       >
         <ResizablePanel defaultSize={70}>
           <div className="flex h-full items-center justify-center p-6">
-            <CitationGraph graphData={graphData} />
+            {search_isLoading || lda_isLoading ? (
+              <div className="flex items-center justify-center">
+                <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-black"></div>
+              </div>
+            ) : (
+              <CitationGraph graphData={graphData} />
+            )}
           </div>
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel minSize={20} defaultSize={30}>
           <div className="flex h-full flex-col items-center justify-center gap-6 p-6">
-            {/* <div className="flex w-full max-w-sm items-center gap-2">
-              <Input
-                type="text"
-                placeholder="Search"
-                onChange={(e) => setSearchInput(e.target.value)}
-              />
-              <Button
-                onClick={() =>
-                  handleLDA(searchInput.split(" "), stopwordInput.split(" "))
-                }
-              >
-                Search
-              </Button>
-            </div>
-
-            <div className="flex w-full max-w-sm items-center gap-2">
-              <Input
-                type="text"
-                placeholder="Additional Stopwords"
-                onChange={(e) => setStopwordInput(e.target.value)}
-              />
-              <Button
-                onClick={() =>
-                  handleLDA(searchInput.split(" "), stopwordInput.split(" "))
-                }
-              >
-                Search
-              </Button>
-            </div> */}
             <InputForm onSubmit={onSubmit} />
           </div>
         </ResizablePanel>
