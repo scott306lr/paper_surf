e26a62b081551bfd0b73fd56b563dbcc2285e7a6
TUTUchen0306 <st993201@gmail.com>
2024-01-04 15:04:42 +0800

    add sweeps

--- src/pages/final.tsx
+++ src/pages/final.tsx
@@ -60,7 +60,7 @@ export default function PaperSurf() {
     if (search_result != null)
       lda_mutate({
         paperID_array: to_lda(search_result),
-        sweeps: 5,
+        sweeps: 10,
         stopwords: values.stopwords.split(", "),
       });
   };
