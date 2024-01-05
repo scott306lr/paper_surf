a9774d4b745ffc76989c8fb06ab8c24d849539e5
TUTUchen0306 <st993201@gmail.com>
2024-01-05 12:57:21 +0800

    min max year on color scale

--- src/server/server_utils/fetchHandler.ts
+++ src/server/server_utils/fetchHandler.ts
@@ -42,7 +42,7 @@ const recommend_url =
 
 export const getColor = (year: number, min_year: number, max_year: number) => {
   if (year == -1) return "#000000";
-  const f = chroma.scale(['yellow', '008ae5']).domain([1980, 2024]);
+  const f = chroma.scale(['yellow', '008ae5']).domain([min_year, max_year]);
   return f(year).hex().toString();
 }
 
